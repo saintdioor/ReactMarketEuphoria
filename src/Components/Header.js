b939191ed2d6de8fad6f692fb273878dc35c8046
Vavilkin Yaroslav <vavilkinyaroslav@gmail.com>
2023-10-04 12:41:36 +0300

    modified:   src/App.js 	new file:   src/Components/Header.js 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+    return(
+        <div>
+            <div>
+                <img src='app\src\icon-header.svg' alt=''/>
+            </div>
+            <div>
+                Text
+            </div>
+        </div>
+    );
+}
+
+export default Header;
